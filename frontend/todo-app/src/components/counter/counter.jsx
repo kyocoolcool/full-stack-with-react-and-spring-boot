924d492999fda244fd86344b03381042c5d084e1
ChrisChen陳金昌 <kyocoolcool@hotmail.com>
2020-04-26 14:31:41 +0800

    進階操作直接使用parent method 傳入child參數值

--- frontend/todo-app/src/components/counter/counter.jsx
+++ frontend/todo-app/src/components/counter/counter.jsx
@@ -9,6 +9,7 @@ class Counter extends Component {
             count: 0
         };
         this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
     }
 
     render() {
@@ -16,10 +17,13 @@ class Counter extends Component {
             <div className="counter">
                 {/*透過props實現同一模組傳入不同屬性效果*/}
                 {/*透過props讓child可以呼叫parent method*/}
-                <CounterButton by={1} incrementMethod={this.increment}/>
-                <CounterButton by={5} incrementMethod={this.increment}/>
-                <CounterButton by={10} incrementMethod={this.increment}/>
+                <CounterButton by={1} incrementMethod={this.increment} decrementMethod={this.decrement}/>
+                <CounterButton by={5} incrementMethod={this.increment} decrementMethod={this.decrement}/>
+                <CounterButton by={10} incrementMethod={this.increment} decrementMethod={this.decrement}/>
                 <span className="count" style={{fontSize: "50px"}}>{this.state.count}</span>
+                <div>
+                    <button className={"reset"} onClick={() => this.reset()}>reset</button>
+                </div>
             </div>
         )
     }
@@ -34,21 +38,44 @@ class Counter extends Component {
             }
         )
     }
+
+    decrement(by) {
+        // console.log(`parent be called by child ${by}`);
+        // prev參數是這個component的上一個state
+        this.setState((prev) => {
+                return {
+                    count: prev.count - by
+                }
+            }
+        )
+    }
+
+    reset = () => {
+        this.setState(() => {
+                return {
+                    count: 0
+                }
+            }
+        );
+    }
 }
 
 class CounterButton extends Component {
     constructor() {
         super();
-        this.state = {
-            count: 0
-        };
-        this.increment = this.increment.bind(this);
+        // this.state = {
+        //     count: 0
+        // };
+        // this.increment = this.increment.bind(this);
+        // this.decrement = this.decrement.bind(this);
     }
 
     render() {
         return (
             <div className="counterButton">
-                <button onClick={this.increment}>+{this.props.by}</button>
+                {/*進階操作直接使用parent method 傳入child參數值*/}
+                <button onClick={()=>this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+                <button onClick={()=>this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
                 {/*<button onClick={() => this.show()}>+1</button>*/}
                 {/*<button onClick={this.show}>+1</button>*/}
                 {/*inline css in React */}
@@ -58,16 +85,22 @@ class CounterButton extends Component {
     }
 
     //第一種賦值方式
-    increment() {
-        this.setState({
-            count: this.state.count + this.props.by
-        });
-        this.props.incrementMethod(this.props.by);
-    }
+    // increment() {
+        // this.setState({
+        //     count: this.state.count + this.props.by
+        // });
+    //     this.props.incrementMethod(this.props.by);
+    // }
 
     //第二種賦值方式
-    show = () => this.setState({count: this.state.count + 1}, () => console.log(this.state.count));
+    // show = () => this.setState({count: this.state.count + 1}, () => console.log(this.state.count));
 
+    // decrement() {
+        // this.setState({
+        //     count: this.state.count - this.props.by
+        // });
+    //     this.props.decrementMethod(this.props.by);
+    // }
 }
 
 //預設props
